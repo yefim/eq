ad4cc16d6bb61e5b8a3f73dfae9432be63ed0980
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-05-23 17:38:27 -0700

    Added mongo

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require "sinatra"
+require "mongo"
+require "json/ext"
+
+include Mongo
+
+configure do
+  conn = MongoClient.new('localhost', 27017)
+  set :collection, conn['db']['eq']
+end
 
 get "/" do
   erb :index
+end
+
+get "/questions/:page" do |page|
+  content_type :json
+  settings.collection.find(page: page).to_a.to_json
 end
